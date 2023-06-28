b19ff7ac40be75ac6a386a54465a6faf6e28a494
Lawrence Nno <lawrence.nno@gmail.com>
2023-06-28 15:30:11 +0100

    Added the pascal's function

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    """
+    Pascal's Trangle function
+    """
+    ans = []
+    final_list = []
+    if n < 1:
+        print(ans)
+    for i in range(1, n + 1):
+        c = 1
+        ans = [c]
+        for j in range(1, i):
+            c = c * (i - j) // j
+            ans.append(c)
+        final_list.append(ans)
+    return(final_list)
