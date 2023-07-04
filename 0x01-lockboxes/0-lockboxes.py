93cd03939932976cfd2bf015dd0f0f3aac7e9d56
Lawrence Nno <lawrence.nno@gmail.com>
2023-07-04 12:06:32 +0100

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,20 +6,20 @@ sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 
 
 def canUnlockAll(boxes):
-    """a function to determine if all the boxes can be opened"""
-    if not boxes:
-        return False
-    amount = len(boxes)
-    result = {}
-    index = 0
+  """a function to determine if all the boxes can be opened"""
+  if not boxes:
+      return False
+  amount = len(boxes)
+  result = {}
+  index = 0
 
-    for box in boxes:
-        if len(box) == 0 or index == 0:
-            result[index] = -1
-        for key in box:
-            if key < amount and key != index:
-                result[key] = key
-        if len(result) == amount:
-            return True
-        index += 1
-    return False
+  for box in boxes:
+      if len(box) == 0 or index == 0:
+          result[index] = -1
+      for key in box:
+          if key < amount and key != index:
+              result[key] = key
+      if len(result) == amount:
+          return True
+      index += 1
+  return False
