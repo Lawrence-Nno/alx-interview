390950dbf620cf2ab12be49cef4f0029b6ba4e1c
Lawrence Nno <lawrence.nno@gmail.com>
2023-07-12 14:34:13 +0100

    Edited my code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,12 +7,14 @@ In a text file, there is a single character H. Your text editor can execute only
 def minOperations(n):
   """A method that returns the fewest num of operations"""
   num_of_operations = 0
+  index = 2
   if n <= 1:
     return 0
 
-  for i in range(2, n + 1):
+  while (index < n + 1):
     """checks if n is divisible"""
-    while n % i == 0:
-      n = n / i
-      num_of_operations += 1
+    while n % index == 0:
+      num_of_operations += index
+      n /= index
+    index += 1
   return num_of_operations
